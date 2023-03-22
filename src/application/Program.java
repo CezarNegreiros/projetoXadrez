d46bfe557504abb56fea24105de82df4c21191f0
CezarNegreiros <cezarnegreiros07@gmail.com>
2023-03-22 11:01:21 -0400

    Implementação do Tabuleiro de das Peças

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,12 @@
 package application;
 
-import tabuleiro.Posicao;
+import tabuleiroJogo.Posicao;
+import tabuleiroJogo.Tabuleiro;
 
 public class Program {
     public static void main(String[] args) {
-        Posicao posicao = new Posicao(3, 5);
-        System.out.println(posicao);
+
+        Tabuleiro tabuleiro = new Tabuleiro(8, 8);
+        
     }
 }
